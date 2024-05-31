0ec9a805b6862e4dc44a7c8e61ee37bc07f70bbb
Vanya Dimitrova <vapedim@yahoo.com>
2024-05-31 22:29:44 +0300

    Add 01.personData.js

---
+++ 06.Objects/01.personData.js
@@ -0,0 +1,10 @@
+function personData(name, surname, age) {
+  return {
+    firstName: name,
+    lastName: surname,
+    age: age
+  }
+}
+
+console.log(personData('Ivan','Ivanov', 29));
+console.log(personData('Maria','Marinova', 13));
