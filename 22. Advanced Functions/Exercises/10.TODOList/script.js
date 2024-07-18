f7d245c81c469897d510093716137f94c0815843
Vanya Dimitrova <vapedim@yahoo.com>
2024-07-18 22:01:32 +0300

    To-Do List  - create listItem

--- 22. Advanced Functions/Exercises/10.TODOList/script.js
+++ 22. Advanced Functions/Exercises/10.TODOList/script.js
@@ -1,7 +1,10 @@
 document.getElementById('add-button').addEventListener('click', function() {
     const input = document.getElementById('todo-input');
     const task = input.value.trim();
-    
-    
-    }
-  );
+
+    const list = document.getElementById('todo-list');
+    const listItem = document.createElement('li');
+    listItem.textContent = task;
+    list.appendChild(listItem);
+
+});
