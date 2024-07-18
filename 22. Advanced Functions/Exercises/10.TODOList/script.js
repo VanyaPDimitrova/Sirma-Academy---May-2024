ddbba47c2fc537832afa48eaba434ae9b00cc2b2
Vanya Dimitrova <vapedim@yahoo.com>
2024-07-18 22:00:11 +0300

    To-Do List  initial commit

---
+++ 22. Advanced Functions/Exercises/10.TODOList/script.js
@@ -0,0 +1,7 @@
+document.getElementById('add-button').addEventListener('click', function() {
+    const input = document.getElementById('todo-input');
+    const task = input.value.trim();
+    
+    
+    }
+  );
