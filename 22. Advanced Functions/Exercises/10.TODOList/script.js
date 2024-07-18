6e82f275c9288d7a8140bb0beec80bb0bfad864e
Vanya Dimitrova <vapedim@yahoo.com>
2024-07-18 22:48:08 +0300

    verify if the input field is not empty when create listItem and clear it when done

--- 22. Advanced Functions/Exercises/10.TODOList/script.js
+++ 22. Advanced Functions/Exercises/10.TODOList/script.js
@@ -1,10 +1,16 @@
 document.getElementById('add-button').addEventListener('click', function() {
     const input = document.getElementById('todo-input');
     const task = input.value.trim();
+    
 
-    const list = document.getElementById('todo-list');
-    const listItem = document.createElement('li');
-    listItem.textContent = task;
-    list.appendChild(listItem);
+    if (input.value != '') {
+        const list = document.getElementById('todo-list');
 
+        const listItem = document.createElement('li');
+        listItem.classList.add("list-item");
+        listItem.textContent = task;
+        list.appendChild(listItem);
+        input.value = '';
+
+    }
 });
